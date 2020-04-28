51506847bc95e56388ffcdd684c6cba4b85c4436
RickyGomez10 <00108517@uca.edu.sv>
2020-04-28 15:00:49 -0600

    Tarea 3 labos 00108517

---
+++ LaboTarea3/src/main/java/com/example/uca/LaboTarea3/controller/MainController.java
@@ -0,0 +1,46 @@
+package com.example.uca.LaboTarea3.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.uca.LaboTarea3.domain.Product;
+
+@Controller
+public class MainController {
+	
+	private List<Product> productos = new ArrayList<Product>();
+	
+	@GetMapping("/compraProducto")
+	public ModelAndView compraProducto() {
+		productos.clear();
+	ModelAndView mav = new ModelAndView();
+	
+	productos.add(new Product(0, "GTA5", 10));
+	productos.add(new Product(1, "Papitas", 20));
+	productos.add(new Product(2, "Chocolates", 30));
+	productos.add(new Product(3, "Dulces", 40));
+	mav.setViewName("select");
+	mav.addObject("product", new Product());
+	mav.addObject("producto", productos);
+	
+	return mav;
+	}
+	
+	@PostMapping("/validar")
+	public String validar(Product producto) {
+		if( producto.getCantidad() > productos.get(producto.getId()).getCantidad()) {
+			return "error";
+		}else {
+			return "aceptado";
+				
+		}
+		
+		
+		
+	}
+}
---
+++ LaboTarea3/src/test/java/com/example/uca/LaboTarea3/LaboTarea3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uca.LaboTarea3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboTarea3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
